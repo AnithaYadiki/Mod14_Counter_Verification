173c9d4a84b748a19b4e2c464fd68cc4cb70a521
Anitha Yadiki <anithayadiki1@gmail.com>
2025-07-23 18:49:52 +0530

    Create dest_xtn.sv

---
+++ dest_xtn.sv
@@ -0,0 +1,11 @@
+class dest_xtn extends uvm_sequence_item;
+	`uvm_object_utils(dest_xtn)
+	 bit [4:0] data_out;
+function new(string name ="source_xtn");
+	super.new(name);
+endfunction
+function void do_print(uvm_printer printer);
+	printer.print_field("data_out",this.data_out,4,UVM_DEC);
+endfunction
+endclass
+
